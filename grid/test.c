c6f040901a617ed6324eb900a2ec40cd1b52c9aa
Thibault PARPAITE <Thibault@localhost.localdomain>
2015-03-22 18:16:47 +0100

    Correction de la fonction main dans test.c avec affichage correct des tests.

--- grid/test.c
+++ grid/test.c
@@ -6,39 +6,39 @@
 
 bool test_creation_grille(grid g)
 {
-  if(grid_score(g)==0)
-    printf("score initialisé à 0");
-  for(int i=0;i<GRID_SIDE;i++)
-  {
-    for(int j=0;j<GRID_SIDE;j++)
-    {
-      if(get_tile(g,i,j)!=0)
-      {
-		return false;
-      }
+	/*if(grid_score(g)==0)
+	  printf("score initialisé à 0");*/
+	for(int i=0;i<GRID_SIDE;i++)
+		{
+			for(int j=0;j<GRID_SIDE;j++)
+				{
+					if(get_tile(g,i,j)!=0)
+						{
+							return false;
+						}
       
-    }
-  }
+				}
+		}
     return true;
 }
 
    
 bool test_copie(grid g)
 {
-  grid g1=new_grid();
-  copy_grid(g,g1);
-  for(int i = 0 ; i < GRID_SIDE ; i++)
-  {
-    for(int j = 0 ; j < GRID_SIDE ; j++)
-    {
-      if(get_tile(g,i,j) != get_tile(g1,i,j)){
-		return false;
-      }
-    }
-  }
-  if(grid_score(g)==grid_score(g1))
-    return true;
-  return false;
+	grid g1=new_grid();
+	copy_grid(g,g1);
+	for(int i = 0 ; i < GRID_SIDE ; i++)
+		{
+			for(int j = 0 ; j < GRID_SIDE ; j++)
+				{
+					if(get_tile(g,i,j) != get_tile(g1,i,j)){
+						return false;
+					}
+				}
+		}
+	if(grid_score(g)==grid_score(g1))
+		return true;
+	return false;
 }
 
 bool test_add_tile(grid g)
@@ -54,76 +54,76 @@ bool test_do_move(grid g,dir d)
 {
 	do_move(g,d);
 	switch(d)
-    {
-	case UP:
-		for(int i=0;i<GRID_SIDE;i++)
-        {
-			int j=0;
-			while(j<=GRID_SIDE-1 || (get_tile(g,i,j)!=0))
-            {
-				j++;
-            }
-			while(j<=GRID_SIDE-1)  // est ce que c'est bien ca que tu voulais ecrire?
-            {
-				if (get_tile(g,i,j)!=0)
-					return false;
-                j++;
-            }
-        }
+		{
+		case UP:
+			for(int i=0;i<GRID_SIDE;i++)
+				{
+					int j=0;
+					while(j<=GRID_SIDE-1 || (get_tile(g,i,j)!=0))
+						{
+							j++;
+						}
+					while(j<=GRID_SIDE-1)  // est ce que c'est bien ca que tu voulais ecrire?
+						{
+							if (get_tile(g,i,j)!=0)
+								return false;
+							j++;
+						}
+				}
 			return true;
 				
-	case DOWN:
-		for(int i=0;i<=GRID_SIDE;i++)
-        {
-			int j=GRID_SIDE-1;
-			while(j>=0 || (get_tile(g,i,j)!=0))
-            {
-				j--;
-            }
-			while(j>=0)
-            {
-				if (get_tile(g,i,j)!=0)
-					return false;
-                j--;
-            }
-        }
+		case DOWN:
+			for(int i=0;i<=GRID_SIDE;i++)
+				{
+					int j=GRID_SIDE-1;
+					while(j>=0 || (get_tile(g,i,j)!=0))
+						{
+							j--;
+						}
+					while(j>=0)
+						{
+							if (get_tile(g,i,j)!=0)
+								return false;
+							j--;
+						}
+				}
 			return true;
 				
 
-	case RIGHT:
-		for(int j=0;j<=GRID_SIDE;j++)
-        {
-			int i=0;
-			while(i<GRID_SIDE || (get_tile(g,i,j)!=0))
-            {
-				i++;
-            }
-			while(i<GRID_SIDE)
-            {
-				if (get_tile(g,i,j)!=0)
-					return false;
-                i++
-            }
-        }
+		case RIGHT:
+			for(int j=0;j<=GRID_SIDE;j++)
+				{
+					int i=0;
+					while(i<GRID_SIDE || (get_tile(g,i,j)!=0))
+						{
+							i++;
+						}
+					while(i<GRID_SIDE)
+						{
+							if (get_tile(g,i,j)!=0)
+								return false;
+							i++;
+						}
+				}
 			return true;
 		
-	case LEFT:
-		for(int j=0;j<=GRID_SIDE;j++)
-        {
-			int i=GRID_SIDE-1;
-			while(i>=0 || (get_tile(g,i,j)!=0))
-            {
-				i--;
-            }
-			while(i>=0)
-            {
-				if (get_tile(g,i,j)!=0)
-					return false;
-                i--;
-            }
-        }
+		case LEFT:
+			for(int j=0;j<=GRID_SIDE;j++)
+				{
+					int i=GRID_SIDE-1;
+					while(i>=0 || (get_tile(g,i,j)!=0))
+						{
+							i--;
+						}
+					while(i>=0)
+						{
+							if (get_tile(g,i,j)!=0)
+								return false;
+							i--;
+						}
+				}
 			return true;
-}
+		}
 }
 
 
@@ -136,41 +136,57 @@ bool test_masque_can_move (grid g, int imin, int imax, int jmin, int jmax, int i
 
 
 bool test_can_move(grid g,dir d)
-    {
+{
 	switch(d)
-    {
-     case UP:
+		{
+		case UP:
      		return test_masque_can_move(g, 0, GRID_SIDE, 1, GRID_SIDE, 0, -1);
-	case DOWN:
+		case DOWN:
      		return test_masque_can_move(g, 0, GRID_SIDE, 0, GRID_SIDE-1,0,1);	
-	case LEFT:
+		case LEFT:
      		return test_masque_can_move(g, 1, GRID_SIDE, 0, GRID_SIDE, -1, 0);
-	case RIGHT:
+		case RIGHT:
      		return test_masque_can_move(g,0,GRID_SIDE-1,0,GRID_SIDE,1,0);
-	}
-  }
-
-int  main(){
-    grid g=new_grid();
-    return test_creation_grille(g);
-    return test_copie(g);
-    dir d=UP;
-    return test_can_move(g,d);
-    if(can_move(g,d))
-        return test_do_move(g,d);
-    d=DOWN;
-    return test_can_move(g,d);
-    if(can_move(g,d))
-        return test_do_move(g,d);
-    d=LEFT;
-    return test_can_move(g,d);
-    if(can_move(g,d))
-        return test_do_move(g,d);
-    d=RIGHT;
-    return test_can_move(g,d);
-    if(can_move(g,d))
-        return test_do_move(g,d);
-    return test_add_tile(g);
-    
-    return 0;
+		}
 }
+
+bool test_do_move_all (grid g) {
+    dir t[] = {UP, DOWN, LEFT, RIGHT};
+	for (int i = 0 ; i < 4 ; i++) {
+		if(can_move(g,t[i]) && !test_do_move(g,t[i]))
+			return false;
+	}
+}
+
+bool test_can_move_all (grid g) {
+    dir t[] = {UP, DOWN, LEFT, RIGHT};
+	for (int i = 0 ; i < 4 ; i++) {
+		if(can_move(g,t[i]) && !test_can_move(g,t[i]))
+			return false;
+	}
+}
+
+	/*   d = DOWN;
+		 return test_can_move(g,d);
+		 if(can_move(g,d))
+		 return test_do_move(g,d);
+		 d=LEFT;
+		 return test_can_move(g,d);
+		 if(can_move(g,d))
+		 return test_do_move(g,d);
+		 d=RIGHT;
+		 return test_can_move(g,d);
+		 if(can_move(g,d))
+		 return test_do_move(g,d);*/
+
+	int main(){
+		grid g = new_grid();
+
+		printf("Test de la fonction create_grid : %s\n", test_creation_grille(g)?"True":"False");
+		printf("Test de la fonction copy : %s\n", test_copie(g)?"True":"False");
+		printf("Test de la fonction can_move : %s\n", test_can_move_all(g)?"True":"False");
+		printf("Test de la fonction do_move : %s\n", test_do_move_all(g)?"True":"False");
+		printf("Test de la fonction add_tile : %s\n", test_add_tile(g)?"True":"False");
+    
+		return EXIT_SUCCESS;
+	}
