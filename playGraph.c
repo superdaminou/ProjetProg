ed447af39782e4fd9edebc5e3d409d7c629b97f2
Thibault PARPAITE <Thibault@localhost.localdomain>
2015-04-22 17:23:19 +0200

    Correction include SDL

--- playGraph.c
+++ playGraph.c
@@ -1,7 +1,7 @@
 #include "grid.h"
 #include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
-#include <SDL2_ttf/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 // Affichage de la grille et du score (version basique à améliorer)
 
